cc62ec5ee5978f9cab900a69c84abb6f63b116ff
korede <korede@yahoo.com>
2019-05-25 11:53:54 +0100

    pasted my graph algorithm to return reward to frontend

--- routes/reward.js
+++ routes/reward.js
@@ -8,7 +8,89 @@ router.get('/', function(req, res, next) {
 });
 
 
+router.post('/upload',function(req,res, next){
+	
+	var a = {};
 
+	class Graph{
+		constructor(){
+			this.graph={}
+		}
+		
+		addEdge(u,v){
+			if(this.graph.hasOwnProperty(u)){
+				this.graph[u].push(v);
+			}
+			else{
+				this.graph[u]=[v];
+			}
+		}
+		
+		pnts(s){
+			var visited = {};
+			var i;
+			for (i in this.graph){
+				visited[i]= false;
+			}
+			var queue=[];
+			queue.push([s,0]);
+			visited[s]=true;
+			var j = 0;
+			var points = 0;
+			while(queue.length){
+				s = queue.shift()
+				if(s[0] != null){ 
+					points+= s[1];
+				}
+				for(var i=0; i<this.graph[s[0]].length; i++){
+					if(this.graph[s[0]][i] != null && visited[this.graph[s[0]][i]] == false){
+						queue.push([this.graph[s[0]][i],Math.pow(1/2,j)]);
+						visited[this.graph[s[0]][i]] = true;
+					}
+				}
+				j++;
+			}
+			return points;
+		}
+	}
+
+	function lint(i){
+	
+		var x = /([\S]*) recommends ([\S]*)/.exec(i);
+
+		if(x != null){
+			if( !a.hasOwnProperty(x[2])){
+				a[x[2]]=x[1];
+			}
+		}
+		else{
+			x = /([\S]*) accepts/.exec(i);
+			if(a.hasOwnProperty(x[1])){
+				g.addEdge(a[x[1]],x[1]);
+				g.addEdge(x[1],null);
+			}
+		}
+	}
+
+	var g = new Graph();
+	var inputData = req.files.inputFile;
+	var lineReader = readline.createInterface({
+	  input: fs.createReadStream(inputData.tempFilePath)
+	});
+
+	lineReader.on('line', (input) => {lint(input);});
+
+	lineReader.on(
+		'close', 
+		function(){
+			var b = {};
+			var i;
+			for(i in g.graph){
+				var t = g.pnts(i);
+				if(t != 0){
+					b[i]=t;
+				}
+			}
 			// console.log(b);
 			// res.send(b);
 			res.render('index', { reward: JSON.stringify(b)  , title : "Customer Review System" });
